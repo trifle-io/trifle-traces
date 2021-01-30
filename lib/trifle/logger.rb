6a6b787e50e04db1b69af8c8956cf2ef22c18800
Jozef Vaclavik <jozef@hey.com>
2021-01-30 08:06:42 +0100

    Initial commit

---
+++ lib/trifle/logger.rb
@@ -0,0 +1,8 @@
+require "trifle/logger/version"
+
+module Trifle
+  module Logger
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/trifle/logger_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Trifle::Logger do
+  it "has a version number" do
+    expect(Trifle::Logger::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
